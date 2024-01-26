72f92d5154a47782bd1e05496591230ec528a652
thanmayi565 <154117587+thanmayi565@users.noreply.github.com>
2024-01-26 08:08:43 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,5 +1,5 @@
 
-#variable "terraforms3bucket2023" {
+#variable "terraforms3bucket2024" {
 # description = "terraform_s3_bucket"
 # default     = "terraforms3bucket2023" # Change this to your desired S3 bucket name
 #}
