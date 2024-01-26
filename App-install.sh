5dbeba9972495c6398ea8447263e0f2e4e4d1a32
thanmayi565 <154117587+thanmayi565@users.noreply.github.com>
2024-01-26 08:06:00 +0530

    Update App-install.sh

--- App-install.sh
+++ App-install.sh
@@ -4,7 +4,7 @@ sudo yum update -y
 sudo yum install -y httpd
 sudo systemctl enable httpd
 sudo service httpd start  
-sudo echo '<h1>Welcome to DevOps-Yogi - APP-1</h1>' | sudo tee /var/www/html/index.html
+sudo echo '<h1>Welcome to DevOps-Thanmayi - APP-1</h1>' | sudo tee /var/www/html/index.html
 sudo mkdir /var/www/html/app1
 sudo echo '<!DOCTYPE html>   
 <html>   
@@ -53,7 +53,7 @@ button {
 </style>   
 </head>    
 <body>    
-    <center> <h1> DevOps-Yogi APP Login </h1> </center>   
+    <center> <h1> DevOps-Thanmayi APP Login </h1> </center>   
     <form>  
         <div class="container">   
             <label>Username : </label>   
